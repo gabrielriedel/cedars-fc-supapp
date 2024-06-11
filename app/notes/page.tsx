16f4010e88f2ec8dc64d8da5b17f8dab7f16e480
gabrielriedel <gaberiedel@gmail.com>
2024-06-11 15:01:59 -0700

    Adding notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,15 @@
 import { createClient } from '@/utils/supabase/server'
 
 export default async function Page() {
-  const supabase = createClient()
-  const { data: guests } = await supabase.from('guests').select()
-
-  return <pre>{JSON.stringify(guests, null, 2)}</pre>
+  const supabase = createClient();
+  let monday;
+  try {
+    let { data: mondayData, error } = await supabase.from('monday_acts').select();
+    if (error) throw error;
+    monday = mondayData;
+  } catch (err) {
+    console.error('Error fetching notes:', err);
+    return <pre>Error loading data!</pre>;
+  }
+  return <pre>{JSON.stringify(monday, null, 2)}</pre>;
 }
