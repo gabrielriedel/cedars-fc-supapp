3cdff6c856b200970b202dbd7a8cf121fdea4faf
gabrielriedel <gaberiedel@gmail.com>
2024-08-04 12:40:55 -0500

    fixed view layout

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -19,7 +19,7 @@ const Page: React.FC = () => {
 
     return (
       <div className="flex w-full min-h-screen bg-white text-green-800">
-          <div className="flex flex-col pl-20 pt-8 w-3/5">
+          <div className="flex flex-col pl-20 pt-8 w-full">
               <Link
                   href="/protected"
                   className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
@@ -55,33 +55,8 @@ const Page: React.FC = () => {
                   />
               ))}
           </div>
-          {/* Right Column for Instructions */}
-          <div className="w-2/5 bg-green-100 p-8 text-green-800">
-              <h2 className="text-2xl font-bold mb-4">Instructions</h2>
-              <p className="text-lg leading-relaxed">
-                  Welcome to the Cedars Family Camp registration system! Here you can select activities for each day and hour.
-                  Follow these steps to ensure your party is ready for a fun-filled summer:
-              </p>
-              <ul className="list-disc pl-4 mt-4">
-                  <li>Select a party member from the dropdown on the left.</li>
-                  <li>Choose the day and hour to view available activities.</li>
-                  <li>Click on an activity to register the selected party member.</li>
-                  <li>If you change your mind on your choice of activity, you may select a different one and the change will be made.</li>
-                  <li>Refresh the page for the most up to date "spaces left" count</li>
-                  <li>If there was an activity that you really wanted to participate in but it was full, then leave a message with that activity so we can make an effort to get you in that activity a different day</li>
-              </ul>
-              <p className="mt-4">
-                  If you have any questions or need further assistance, please contact our support team.
-              </p>
-          </div>
       </div>
   );
-  
-  
-  
-    
-    
-    
 };
 
 export default Page;
